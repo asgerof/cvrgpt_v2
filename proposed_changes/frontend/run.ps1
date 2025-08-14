2afcbe169f2489501e2b5eef7420ab4b6444f541
asgerof <asgerof@gmail.com>
2025-08-14 19:07:38 +0200

    Delete proposed_changes directory
    not needed

--- proposed_changes/frontend/run.ps1
+++
@@ -1,9 +0,0 @@
-# frontend/run.ps1
-param(
-  [string]$ApiBase = "http://localhost:8000"
-)
-
-Set-Location -Path "$PSScriptRoot"
-$env:NEXT_PUBLIC_API_BASE_URL = $ApiBase
-if (-not (Test-Path node_modules)) { npm install }
-npm run dev
